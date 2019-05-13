4ff0c8f345a11b97492a9db975bafab795db5341
shldhee <shldhee@yahoo.co.kr>
2019-05-13 23:34:20 +0900

    Working chart

--- src/views/JMTZList.vue
+++ src/views/JMTZList.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <h1>Load Items</h1>
-    <p>{{ getJMTZsToFilter }}</p>
+    <p>{{ dataChart }}</p>
+    <ChartDonut :chartData="dataChart" :options="chartOptions" />
     <table>
       <colgroup>
         <col style="width:" />
@@ -39,17 +40,25 @@
 </template>
 
 <script>
+import ChartDonut from '@/components/ChartDonut'
 export default {
-  data() {
-    return {
-      newJMTZ: []
+  components: {
+    ChartDonut
+  },
+  computed: {
+    dataChart() {
+      let dataObject = {
+        labels: [],
+        datasets: []
+      }
+      let newArray = []
+      Object.keys(this.getJMTZs).forEach(prop => {
+        newArray.push(this.getJMTZs[prop].category)
+      })
+
+      dataObject.labels = newArray
+      return dataObject
     }
-  },
-  created() {
-    console.log('Created!!')
-  },
-  beforeRouteUpdate() {
-    console.log('beforeRouteUpdate', this.getJMTZs)
   },
   props: {
     getJMTZs: {
@@ -59,15 +68,31 @@ export default {
         return {}
       }
     }
-  },
-  computed: {
-    getJMTZsToFilter() {
-      let newArray = []
-      Object.keys(this.getJMTZs).forEach(prop => {
-        newArray.push(this.getJMTZs[prop].category)
-      })
-      return newArray
-    }
   }
 }
+
+// data: {
+//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12, 19, 3, 5, 2, 3],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)',
+//                 'rgba(75, 192, 192, 0.2)',
+//                 'rgba(153, 102, 255, 0.2)',
+//                 'rgba(255, 159, 64, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255, 99, 132, 1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
 </script>
